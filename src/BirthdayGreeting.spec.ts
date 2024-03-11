6e839aa3fa169acfb0bcc8dd903cfeaf084a7c43
Pablo400-glitch <alu0101311135@ull.edu.es>
2024-03-11 12:35:42 +0000

    implements the mail sender

--- src/BirthdayGreeting.spec.ts
+++ src/BirthdayGreeting.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest"
-import { BirthdayGreeting } from "./BirthdayGreeting.js"
+import { BirthdayGreeting, DefaultMailer } from "./BirthdayGreeting.js"
 
 describe("Birthday Greetings", () => {
     it("display Happy Birthday", () => {
@@ -9,4 +9,12 @@ describe("Birthday Greetings", () => {
   
       expect(birthday).toBe('Happy birthday!')
     })
+    it("display that the email was send", () => {
+        const defaultMailer = new DefaultMailer()
+        const email = 'paisficretpablo@gmail.com'
+        
+        const mailer = defaultMailer.sendEmail(email)
+    
+        expect(mailer).toBe('Email was send')
+      })
   })
--- src/BirthdayGreeting.ts
+++ src/BirthdayGreeting.ts
@@ -1,3 +1,19 @@
+interface Email {
+    email: string
+}
+
+interface BirthdayMailer {
+    sendEmail(emailString : string) : string
+}
+
+export class DefaultMailer implements BirthdayMailer {
+    sendEmail(emailString : string) : string {
+        const email : Email = {
+            email: emailString
+        };
+        return 'Email was send'
+    }
+}
 
 export class BirthdayGreeting {
     happyBirthday() {
