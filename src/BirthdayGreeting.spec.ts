8af7009246cf69227861267f82a7913450167e31
Pablo400-glitch <alu0101311135@ull.edu.es>
2024-03-12 11:19:05 +0000

    Multiple users birthday

--- src/BirthdayGreeting.spec.ts
+++ src/BirthdayGreeting.spec.ts
@@ -1,54 +1,65 @@
 import { describe, it, expect } from "vitest"
-import { BirthdayGreeting, DefaultMailer, BirthdayBoyDataFile } from "./BirthdayGreeting.js"
+import { BirthdayGreeting, DefaultMailer, BirthdayBoyDataFile} from "./BirthdayGreeting.js"
 require('dotenv').config();
-import * as fs from 'fs';
 
 const filename = '/home/pablo/Example_Katas/src/birthdayboys.txt'
 
-const content = fs.readFileSync(filename, 'utf-8');
-  
-const data = content.split(', ');
-  
-const date = new Date(data[2])
-const formatedDate = date.toISOString()
-  
-const birthdayBoy = {
-    last_name: data[0],
-    first_name: data[1],
-    date_of_birth: new Date(formatedDate),
-    email: {
-        emailName: data[3],
-    },
-}
-
-const mailOptions = {
-    from: {
-        name: 'Birthday',
-        address: process.env.EMAIL
-    },
-    to: birthdayBoy.email.emailName,
-    subject: "Happy Birthday", // Subject line
-    html: `<p>Happy birthday, dear <strong>${birthdayBoy.first_name} ${birthdayBoy.last_name}</strong>!</p>`,
-};
-
+const birthdayBoy = new BirthdayBoyDataFile()
+const birthdayBoys = birthdayBoy.getData(filename)
 
 describe("Birthday Greetings", () => {
   it("display Happy Birthday on the Email options", () => {
     const birthdayGreeting = new BirthdayGreeting()
 
-    const birthday = birthdayGreeting.happyBirthday(birthdayBoy)
+    birthdayBoys.map((boy) => {
+      const birthday = birthdayGreeting.happyBirthday(boy)
+      const mailOptions = {
+        from: {
+            name: 'Birthday',
+            address: process.env.EMAIL
+        },
+        to: boy.email.emailName,
+        subject: "Happy Birthday", // Subject line
+        html: `<p>Happy birthday, dear <strong>${boy.first_name} ${boy.last_name}</strong>!</p>`,
+      };
 
-    expect(birthday).toBe("Is not your Birthday")
+      expect(birthday).toStrictEqual(mailOptions)
+    })
   })
 
   it("display that the email was send", async () => {
     const defaultMailer = new DefaultMailer()
+    const emailPromises: Promise<string>[] = [];
+
+    birthdayBoys.forEach(async (boy) => {
+      const mailOptions = {
+        from: {
+          name: 'Birthday',
+          address: process.env.EMAIL
+        },
+        to: boy.email.emailName,
+        subject: "Happy Birthday", // Subject line
+        html: `<p>Happy birthday, dear <strong>${boy.first_name} ${boy.last_name}</strong>!</p>`,
+      };
+        
+      const promise = defaultMailer.sendEmail(mailOptions, boy)
+      .then(result => {
+        expect(result).toBe("Email was sent successfully");
+        return result;
+      })
+      .catch(error => {
+        throw error;
+      });
+
+      emailPromises.push(promise);
+    });
 
     try {
-        const result = await defaultMailer.sendEmail(mailOptions);
-        expect(result).toBe('Email has been sent!');
+      const results = await Promise.all(emailPromises);
+  
+      expect(results.every(result => result === "Email was sent successfully")).toBe(true);
     } catch (error) {
-        expect(error instanceof Error).toBe(true);
+      expect(error instanceof Error).toBe(true);
     }
   })
 
@@ -57,6 +68,6 @@ describe("Birthday Greetings", () => {
 
     const birthday = birthdayGreeting.getData(filename)
 
-    expect(birthday).toStrictEqual(birthdayBoy)
+    expect(birthday).toStrictEqual(birthdayBoys)
   })
 })
--- src/BirthdayGreeting.ts
+++ src/BirthdayGreeting.ts
@@ -16,7 +16,7 @@ interface EmailOptions {
   html: string
 }
 
-interface BirthdayBoyData {
+export interface BirthdayBoyData {
   last_name: string
   first_name: string
   date_of_birth: Date
@@ -24,34 +24,43 @@ interface BirthdayBoyData {
 }
 
 interface GetBirthdayBoyData {
-  getData(filename: string): BirthdayBoyData
+  getData(filename: string): BirthdayBoyData[]
 }
 
 interface BirthdayMailer {
-  sendEmail(mailOptions: EmailOptions): Promise<string>
+  sendEmail(mailOptions: EmailOptions, birthdaysData: BirthdayBoyData): Promise<string>
 }
 
 export class BirthdayBoyDataFile implements GetBirthdayBoyData {
-    getData(filename: string): BirthdayBoyData {
-      const content = fs.readFileSync(filename, 'utf-8');
-  
-      const data = content.split(', ');
-  
-      const date = new Date(data[2])
-      const formatedDate = date.toISOString()
-  
-      const BirthdayBoy = {
-        last_name: data[0],
-        first_name: data[1],
-        date_of_birth: new Date(formatedDate),
-        email: {
-          emailName: data[3],
-        },
+  getData(filename: string): BirthdayBoyData[] {
+    const content = fs.readFileSync(filename, 'utf-8');
+    const lines = content.split('\n');
+
+    const birthdayBoys: BirthdayBoyData[] = [];
+
+    for (const line of lines) {
+      const data = line.split(', ');
+
+      if (data.length >= 4) { // Verifica que haya suficientes elementos en la línea
+        const date = new Date(data[2]);
+        const formatedDate = date.toISOString();
+
+        const birthdayBoy = {
+          last_name: data[0],
+          first_name: data[1],
+          date_of_birth: new Date(formatedDate),
+          email: {
+            emailName: data[3],
+          },
+        };
+
+        birthdayBoys.push(birthdayBoy);
       }
-  
-      return BirthdayBoy
     }
+
+    return birthdayBoys;
   }
+}
 
 export class BirthdayGreeting {
   happyBirthday(birthdaysData: BirthdayBoyData) {
@@ -64,16 +73,13 @@ export class BirthdayGreeting {
       subject: "Happy Birthday", // Subject line
       html: `<p>Happy birthday, dear <strong>${birthdaysData.first_name} ${birthdaysData.last_name}</strong>!</p>`, 
     }
-    if (birthdaysData.date_of_birth === new Date()) {
-        return mail
-    } else {
-        return "Is not your Birthday"
-    }
+   
+    return mail
   }
 }
 
 export class DefaultMailer implements BirthdayMailer {
-  async sendEmail(mailOptions: EmailOptions): Promise<string> {
+  async sendEmail(mailOptions: EmailOptions, birthdayData: BirthdayBoyData): Promise<string> {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
       service: "gmail",
@@ -88,12 +94,17 @@ export class DefaultMailer implements BirthdayMailer {
       },
     })
 
-    try {
-        await transporter.sendMail(mailOptions)
-        return "Email was sent successfully"
-    } catch (error) {
-        console.error("Error sending email:", error)
-        throw new Error("Failed to send email")
+    if (birthdayData.date_of_birth.getDate() === new Date().getDate() && 
+        birthdayData.date_of_birth.getMonth() === new Date().getMonth()) {
+      try {
+          await transporter.sendMail(mailOptions)
+          return "Email was sent successfully"
+      } catch (error) {
+          console.error("Error sending email:", error)
+          throw new Error("Failed to send email")
+      }
+    } else {
+      throw new Error("Is not your birthday")
     }
   }
 }
