404a01c0f0ce4c20a91c2cf70dd872a56b9727ec
Pablo400-glitch <alu0101311135@ull.edu.es>
2024-04-17 10:31:44 +0100

    FizzBuzz Kata Finished

--- FizzBuzz/src/main.spec.ts
+++ FizzBuzz/src/main.spec.ts
@@ -1,10 +1,28 @@
 import { describe, it, expect } from "vitest"
-import { myFunction } from "./main.js"
+import { FizzBuzz } from "./main.js"
 
-describe("Default test", () => {
-  it("should work", () => {
-    const result = myFunction()
+describe("FizzBuzz test", () => {
+  it("return Fizz", () => {
+    const result = FizzBuzz(3)
 
-    expect(result).toBe(true)
+    expect(result).toBe("Fizz")
+  })
+
+  it("return Buzz", () => {
+    const result = FizzBuzz(5)
+
+    expect(result).toBe("Buzz")
+  })
+
+  it("return FizzBuzz", () => {
+    const result = FizzBuzz(15)
+
+    expect(result).toBe("FizzBuzz")
+  })
+
+  it("return number", () => {
+    const result = FizzBuzz(7)
+
+    expect(result).toBe(7)
   })
 })
--- FizzBuzz/src/main.ts
+++ FizzBuzz/src/main.ts
@@ -1,3 +1,17 @@
-export function myFunction() {
-  return true
+export function FizzBuzz(n: number) {
+  let result;
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0)
+      result = "FizzBuzz"
+    else if (i % 3 === 0)
+      result = "Fizz"
+    else if (i % 5 === 0)
+      result = "Buzz"
+    else 
+      result = i
+
+    console.log(result)
+  }
+
+  return result
 }
