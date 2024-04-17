0806a33dc59b45536aa04c0de93ce74cb50b09a6
Pablo400-glitch <alu0101311135@ull.edu.es>
2024-04-17 10:55:30 +0100

    Anagram-Kata: Test working

--- Anagram/src/main.spec.ts
+++ Anagram/src/main.spec.ts
@@ -1,10 +1,11 @@
 import { describe, it, expect } from "vitest"
-import { myFunction } from "./main.js"
+import { Anagram } from "./main.js"
 
 describe("Default test", () => {
   it("should work", () => {
-    const result = myFunction()
+    const anagram = new Anagram("documenting")
+    const result = anagram.getAnagram()
 
-    expect(result).toBe(true)
+    expect(result).toBe("documenting")
   })
 })
