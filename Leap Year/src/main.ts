355a40953da2e619fde4b8ccb517956eecff4bbb
Pablo400-glitch <alu0101311135@ull.edu.es>
2024-04-19 12:13:49 +0100

    Leap Year Kata: Test fixed

--- Leap Year/src/main.ts
+++ Leap Year/src/main.ts
@@ -6,9 +6,18 @@ export class LeapYear{
   }
 
   isLeapYear(): boolean {
-    if (this.year % 4 === 0) 
-      return true
-    else
+    if (this.year % 100 === 0 && this.year % 400 != 0)
       return false
+
+    if (this.year % 4 != 0)
+      return false
+
+    if (this.year % 4 === 0)
+      return true
+
+    if (this.year % 400 === 0)
+      return true
+
+    return false
   }
 }
