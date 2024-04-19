d5fe41f6ba94d6213cb0ff2243770bbd8ed6f1b9
Pablo400-glitch <alu0101311135@ull.edu.es>
2024-04-19 11:48:49 +0100

    Leap Year Kata: Test fixed

--- Leap Year/src/main.ts
+++ Leap Year/src/main.ts
@@ -1,3 +1,13 @@
-export function myFunction() {
-  return true
+export class LeapYear{
+  private year: number
+
+  constructor(year: number) {
+    this.year = year
+  }
+
+  isLeapYear(): boolean {
+    if (this.year === 1600)
+      return true
+    return false
+  }
 }
