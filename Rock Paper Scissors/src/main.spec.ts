a6343b59ededc8c5ae843c99b855f69ecc1ebce3
Pablo400-glitch <alu0101311135@ull.edu.es>
2024-04-29 12:46:33 +0100

    Rock Paper Scissorsv Kata: Test failed

--- Rock Paper Scissors/src/main.spec.ts
+++ Rock Paper Scissors/src/main.spec.ts
@@ -1,13 +1,22 @@
 import { describe, it, expect } from "vitest"
 import { RockPaperScissors } from "./main.js"
 
-describe("Default test", () => {
-  it("should work", () => {
+describe("RockPaperScissors", () => {
+  it("You won the game", () => {
     const playerElection = "Rock"
     const enemyElection = "Scissors"
     const rockPaperScissors = new RockPaperScissors(playerElection, enemyElection)
     const result = rockPaperScissors.Game()
 
     expect(result).toBe("You won!!!")
+  })
+
+  it("You lost the game", () => {
+    const playerElection = "Rock"
+    const enemyElection = "Scissors"
+    const rockPaperScissors = new RockPaperScissors(playerElection, enemyElection)
+    const result = rockPaperScissors.Game()
+
+    expect(result).toBe("You lose")
   })
 })
