71f6d910d9bb8681b5ff401e3455fd088ff0fbd2
Pablo400-glitch <alu0101311135@ull.edu.es>
2024-04-30 10:50:37 +0100

    Roman Numeral Kata: test fixed

--- Roman_Numeral/src/main.spec.ts
+++ Roman_Numeral/src/main.spec.ts
@@ -2,52 +2,55 @@ import { describe, it, expect } from "vitest"
 import { RomanNumeral } from "./main.js"
 
 describe("RomanNumeral", () => {
-  it("should return I", () => {
-    const romanNumeral = new RomanNumeral(1)
-    const result = romanNumeral.Convert()
 
-    expect(result).toBe('I')
-  })
-
-  it("should return V", () => {
-    const romanNumeral = new RomanNumeral(5)
-    const result = romanNumeral.Convert()
-
-    expect(result).toBe('V')
-  })
-
-  it("should return X", () => {
-    const romanNumeral = new RomanNumeral(10)
-    const result = romanNumeral.Convert()
-
-    expect(result).toBe('X')
-  })
-
-  it("should return L", () => {
-    const romanNumeral = new RomanNumeral(50)
-    const result = romanNumeral.Convert()
-
-    expect(result).toBe('L')
-  })
-
-  it("should return C", () => {
-    const romanNumeral = new RomanNumeral(100)
-    const result = romanNumeral.Convert()
-
-    expect(result).toBe('C')
-  })
-
-  it("should return D", () => {
-    const romanNumeral = new RomanNumeral(500)
-    const result = romanNumeral.Convert()
-
-    expect(result).toBe('D')
-  })
-
-  it("should return M", () => {
-    const romanNumeral = new RomanNumeral(1000)
-    const result = romanNumeral.Convert()
-
-    expect(result).toBe('M')
+  describe("Main Roman Numbers", () => {
+    it("should return I", () => {
+      const romanNumeral = new RomanNumeral(1)
+      const result = romanNumeral.Convert()
+  
+      expect(result).toBe('I')
+    })
+  
+    it("should return V", () => {
+      const romanNumeral = new RomanNumeral(5)
+      const result = romanNumeral.Convert()
+  
+      expect(result).toBe('V')
+    })
+  
+    it("should return X", () => {
+      const romanNumeral = new RomanNumeral(10)
+      const result = romanNumeral.Convert()
+  
+      expect(result).toBe('X')
+    })
+  
+    it("should return L", () => {
+      const romanNumeral = new RomanNumeral(50)
+      const result = romanNumeral.Convert()
+  
+      expect(result).toBe('L')
+    })
+  
+    it("should return C", () => {
+      const romanNumeral = new RomanNumeral(100)
+      const result = romanNumeral.Convert()
+  
+      expect(result).toBe('C')
+    })
+  
+    it("should return D", () => {
+      const romanNumeral = new RomanNumeral(500)
+      const result = romanNumeral.Convert()
+  
+      expect(result).toBe('D')
+    })
+  
+    it("should return M", () => {
+      const romanNumeral = new RomanNumeral(1000)
+      const result = romanNumeral.Convert()
+  
+      expect(result).toBe('M')
+    })
   })
 })
--- Roman_Numeral/src/main.ts
+++ Roman_Numeral/src/main.ts
@@ -1,11 +1,20 @@
 export class RomanNumeral {
   private amount: number
+  private romanNumbers: { [key: number]: string } = { 
+    1: 'I', 
+    5: 'V', 
+    10: 'X', 
+    50: 'L', 
+    100: 'C', 
+    500: 'D', 
+    1000: 'M' 
+  }
 
   constructor(amount: number) {
     this.amount = amount
   }
 
   Convert(): string {
-    return 'I'
+    return this.romanNumbers[this.amount]
   }
 }
